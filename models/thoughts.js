530343735c6d502b348f60d949b7c4ffbd5fcd10
abigailmgarcia <abigailmg31@gmail.com>
2023-07-31 12:39:14 -0500

    added thout user schema

--- models/thoughts.js
+++ models/thoughts.js
@@ -7,7 +7,52 @@ const thoughtSchema = new Schema(
             type: String,
             required: true,
             minlength:[1, "Thought text must be at least 1 charcter long"],
-            maxlength: [280, "Thought text",]
+            maxlength: [280, "Thought text"],
+        },
+        createdAt: {
+            type: Date, 
+            default: Date.now,
+            get: function (timestamp) {
+                    return new Date(timestamp).toLocaleString();
+            },
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: { virtuals: true },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+// reaction schema
+const reactionSchema = new Schema({
+    reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: function (timestamp) {
+            return new Date(timestamp).toLocaleString();
         }
     }
-)
+})
