f9323798bc6e721255022472c9d0e4e21470ab1f
abigailmgarcia <abigailmg31@gmail.com>
2023-07-31 02:45:26 -0500

    added config

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const db = require("./config/connection")
+
+const app = express();
+const PORT = 3001;
+
+const dbName = 'socialNetDB';
+
+app.listen(port, () => {
+    console.log(`API server running on ${PORT}`);
+});
