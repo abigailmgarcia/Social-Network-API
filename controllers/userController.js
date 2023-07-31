22867680cb4a300efae4361858235f63ad0e9113
abigailmgarcia <abigailmg31@gmail.com>
2023-07-31 13:03:24 -0500

    created route with endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const { User, Thought } = require("../models")
 
 
-const getUsers = asyncHandler( async(req, res) => {
+const getUsers = asyncHandler( async (req, res) => {
     res.json({ message: "testing controller function" });
     const users = await User.find();
     res.json({ users });
@@ -10,23 +10,44 @@ const getUsers = asyncHandler( async(req, res) => {
 
 //get one user
 const getSingleUser = async (req, res) => {
-    res.json({ message: "testing controller function for onr user"});
+    try{
+        const user = await User.findOne({ _id: req.params.id }).select("-__v");
+        if (!user){
+            return res.status(404).json({ message: "No user with that id"});
+        }
+        res.json(user)
+    } catch {
+        res.status(500).json(err)
+    }
 };
 
 //post new user
 const createUser =  async (req, res) => {
     try{
         const user = await User.create(req.body);
-        res.json(user)
+        res.json(user);
     } catch (err) {
         res.status(500).json(err);
     }
 };
 
 //put update user
-const updateUser = asyncHandler(async (req, res) => {
-    res.json({ message: `updated: ${req.params.id}`});
-});
+const updateUser = async (req, res) => {
+    try {
+        const updatedUser = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            req.body,
+            { new: true },
+        );
+
+        if (!updateUser) {
+            res.status(404).json({ message: "No user with this id" })
+        }
+        res.json(updatedUser)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+    };
 
 //delete
 const deleteUser = asyncHandler (async (req, res) => {
