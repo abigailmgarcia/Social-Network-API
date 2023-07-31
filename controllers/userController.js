7385e013fbb0f0c7a90d60451d68716a1b238fcc
abigailmgarcia <abigailmg31@gmail.com>
2023-07-31 12:19:32 -0500

    updated user control

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,32 @@
-const getUsers = (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+
+const getUsers = asyncHandler( async(req, res) => {
     res.json({ message: "testing controller function" });
-};
+});
 
 //get one user
-const getSingleUser = (req, res) => {
+const getSingleUser = asyncHandler(async (req, res) => {
     res.json({ message: "testing controller function for onr user"});
-};
+});
 
 //post new user
-const createUser = (req, res) => {
-    res.json({ message: "created new user" });
-};
+const createUser =  asyncHandler(async (req, res) => {
+    if(!req.body.username){
+    res.status(400).json({ message: "missing info" });
+}
+    res.json({ message: "created a new user" });
+});
 
 //put update user
-const updateUser = (req, res) => {
+const updateUser = asyncHandler(async (req, res) => {
     res.json({ message: `updated: ${req.params.id}`});
-};
+});
 
 //delete
-const deleteUser = (req, res) => {
+const deleteUser = asyncHandler (async (req, res) => {
     res.json({ message: `deleted: ${req.params.id}`});
-};
+});
 
 
 module.exports = {
