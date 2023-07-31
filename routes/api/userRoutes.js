d52f9d078e6d4492002bccf3238cb06e4fe0dedc
abigailmgarcia <abigailmg31@gmail.com>
2023-07-31 03:28:12 -0500

    created user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,30 @@
 const router = require("express").Router();
 
+
+//gets users
 router.get("/", (req, res) => {
     res.json({ message: "great job" });
+});
+
+//get one user
+router.get("/", (req, res) => {
+    res.json({ message: "great job"});
+});
+
+//create user
+router.post("/", (req, res) => {
+    res.json({ message: "great work" });
+});
+
+//update user
+router.put("/:id", (req, res) => {
+    res.json({ message: `update user ${req.params.id}` });
+});
+
+//delete user
+
+router.delete("/:id", (req, res) => {
+    res.json({ message: `delete user ${req.params.id}` });
 });
 
 module.exports = router;
