493d8492d8de0d9586209be4c18c982b8eb54bc9
abigailmgarcia <abigailmg31@gmail.com>
2023-07-31 14:29:05 -0500

    fixed user routing

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,7 +35,7 @@ const createUser =  async (req, res) => {
 const updateUser = async (req, res) => {
     try {
         const updatedUser = await User.findOneAndUpdate(
-            { _id: req.params.userId },
+            { _id: req.params.idd },
             req.body,
             { new: true },
         );
