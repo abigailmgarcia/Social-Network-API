c8f1ade626f4e678acd4c94de14e2f92213234ad
abigailmgarcia <abigailmg31@gmail.com>
2023-07-31 12:25:20 -0500

    added express middleware

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require("dotenv").config();
 const db = require("./config/connection");
 const routes = require("./routes");
+//route response error middleware
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 db.once('open', () => {
     app.listen(PORT, () => {
